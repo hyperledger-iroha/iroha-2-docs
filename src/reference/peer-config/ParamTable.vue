54b1b727f570b06c28dfea99201e25b7549f5911
quacumque <quacumque@fastmail.jp>
2025-04-07 10:46:54 +0900

    chore: format
    Signed-off-by: quacumque <quacumque@fastmail.jp>

--- src/reference/peer-config/ParamTable.vue
+++ src/reference/peer-config/ParamTable.vue
@@ -1,25 +1,17 @@
 <script setup lang="ts">
-import { useSlots } from "vue";
+import { useSlots } from 'vue'
 
 const props = defineProps<{
-  env?: string;
-  type?:
-    | "string"
-    | "file-path"
-    | "public-key"
-    | "socket-addr"
-    | "number"
-    | "millis"
-    | "bytes"
-    | "bool";
-  defaultValue?: string;
-  defaultNote?: string;
-}>();
+  env?: string
+  type?: 'string' | 'file-path' | 'public-key' | 'socket-addr' | 'number' | 'millis' | 'bytes' | 'bool'
+  defaultValue?: string
+  defaultNote?: string
+}>()
 
-const slots = useSlots();
+const slots = useSlots()
 
 function showDefault() {
-  return props.defaultValue || !!slots["default-value"];
+  return props.defaultValue || !!slots['default-value']
 }
 </script>
 
@@ -32,30 +24,52 @@ function showDefault() {
         </th>
         <td>
           <slot name="type">
-            <template v-if='type === "string"'>String</template>
-            <template v-else-if='type === "file-path"'>String, file path (relative to the config file or CWD)</template>
-            <template v-else-if='type === "public-key"'>String, public key multihash</template>
-            <template v-else-if='type === "socket-addr"'>String, socket address (host/IPv4/IPv6 + port)</template>
-            <template v-else-if='type === "number"'>Number</template>
-            <template v-else-if='type === "millis"'>Number, duration in milliseconds</template>
-            <template v-else><code>todo {{ type }}</code></template>
+            <template v-if="type === 'string'">
+              String
+            </template>
+            <template v-else-if="type === 'file-path'">
+              String, file path (relative to the config file or CWD)
+            </template>
+            <template v-else-if="type === 'public-key'">
+              String, public key multihash
+            </template>
+            <template v-else-if="type === 'socket-addr'">
+              String, socket address (host/IPv4/IPv6 + port)
+            </template>
+            <template v-else-if="type === 'number'">
+              Number
+            </template>
+            <template v-else-if="type === 'millis'">
+              Number, duration in milliseconds
+            </template>
+            <template v-else>
+              <code>todo {{ type }}</code>
+            </template>
           </slot>
         </td>
       </tr>
 
-      <tr class="transposed-table" v-if="showDefault()">
+      <tr
+        v-if="showDefault()"
+        class="transposed-table"
+      >
         <th>
           <strong>Default:</strong>
         </th>
         <td>
           <slot name="default-value">
             <code>{{ defaultValue }}</code>
-            <template v-if="defaultNote"> ({{ defaultNote }})</template>
+            <template v-if="defaultNote">
+              ({{ defaultNote }})
+            </template>
           </slot>
         </td>
       </tr>
 
-      <tr class="transposed-table" v-if="env">
+      <tr
+        v-if="env"
+        class="transposed-table"
+      >
         <th>
           <strong>Env:</strong>
         </th>
